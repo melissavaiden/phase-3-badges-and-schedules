6612c475f501b062914b3aa1cb5318224379972d
Melissa Vaiden <melissavaiden@gmail.com>
2022-08-27 12:59:38 -0400

    commit tree 11aebdd1cddacf973f033f5492da2b8ff16dc4a9

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+require "pry"
+
+def badge_maker str
+    "Hello, my name is #{str}."
+end
+
+def batch_badge_creator(names)
+   names.map do |name|
+    badge_maker name
+    end
+end
+
+def assign_rooms array
+    new_array = []
+    array.each.with_index(1) do |name, index|
+       new_array << "Hello, #{name}! You'll be assigned to room #{index}!"
+    end
+    new_array
+end
+
+def printer array
+    batch_badge_creator array
+    assign_rooms array
+end
